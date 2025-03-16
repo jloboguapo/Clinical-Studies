9727b0b8d2f91248400af08804f3db973360715b
Juice J <juicej@Alphas-MacBook-Pro.local>
2025-03-16 14:04:36 -0700

    separate into VCT and nonVCT

--- server.js
+++ server.js
@@ -88,75 +88,6 @@ const getCombinedStudies = () => {
     return `CELERION STUDIES\n\n\n\n${celerionStudies}`;
   };
 
-  const callFortrea = async () => {
-    const response = await fetch(
-      'https://www.fortreaclinicaltrials.com/en-us/clinical-research/browse-studies?field_location_target_id=All&field_gender_target_id=10&field_age_target_id=18'
-    );
-    const data = await response.text();
-    const dom = new JSDOM(data);
-
-    const getHreflang = () =>
-      Array.from(dom.window.document.querySelectorAll('.views-field > a'))
-        .map(name => name.innerHTML)
-        .slice(6);
-
-    const hreflang = getHreflang();
-
-    const getStudies = () =>
-      Array.from(dom.window.document.querySelectorAll('.views-field'))
-        .map(name => name.innerHTML)
-        .slice(6);
-
-    const preSlicedStudies = getStudies();
-
-    const slicedStudies = arr => {
-      const slicedArrays = [];
-      arr.map(
-        (_, index) =>
-          (index % 6 === 0 || index === 0) &&
-          slicedArrays.push(arr.slice(index, index + 6))
-      );
-
-      return slicedArrays;
-    };
-
-    const noHreflang = slicedStudies(preSlicedStudies)
-      .filter(
-        innerArray =>
-          !innerArray.toString().toLowerCase().includes('overweight')
-      )
-      .map(innerArray =>
-        innerArray.filter(
-          name => !name.toString().toLowerCase().includes('hreflang')
-        )
-      );
-
-    const sortDollarAmountsDescending = arr => {
-      arr.sort((a, b) => {
-        const amountA = parseFloat(a.at(-1).replace(/[^0-9.-]+/g, ''));
-        const amountB = parseFloat(b.at(-1).replace(/[^0-9.-]+/g, ''));
-        return amountB - amountA;
-      });
-      return arr;
-    };
-
-    const filteredStudies = (sourceArray, targetArrays) => {
-      sourceArray.map((_, index) =>
-        targetArrays[index].unshift(sourceArray[index])
-      );
-
-      return sortDollarAmountsDescending(targetArrays)
-        .map(innerArr =>
-          innerArr.map(name => name.replaceAll('amp;', '')).join('\n')
-        )
-        .join('\n\n\n');
-    };
-
-    const fortreaStudies = filteredStudies(hreflang, noHreflang);
-
-    return `FORTREA STUDIES\n\n\n\n${fortreaStudies}`;
-  };
-
   const callDrVince = async () => {
     const response = await fetch('https://drvince.com/participate-in-a-study/');
     const data = await response.text();
@@ -193,7 +124,7 @@ const getCombinedStudies = () => {
     return `DR. VINCE STUDIES\n\n\n\n${drVinceStudies}`;
   };
 
-  Promise.allSettled([callCelerion(), callFortrea(), callDrVince()])
+  Promise.allSettled([callCelerion(), callDrVince()])
     .then(values => values)
     .then(result => {
       const msgText = result
@@ -203,7 +134,7 @@ const getCombinedStudies = () => {
       const msg = {
         from: email,
         to: email,
-        subject: 'Available Studies from Celerion, Fortrea, and Dr. Vince',
+        subject: 'Available Studies from Celerion and Dr. Vince',
         text: msgText,
       };
       msgText.length && mg.messages.create(process.env.DOMAIN, msg);
